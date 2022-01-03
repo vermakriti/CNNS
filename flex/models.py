397ff9ef73f985b8851d45fb0d6d205745264840
vermakriti <kriti.verma.it@gmail.com>
2022-01-03 14:38:17 +0530

    added child pages

--- flex/models.py
+++ flex/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
+from streams import blocks
 
 # Create your models here.
 class FlexPage(Page):
 
     template = "flex/flex_page.html"
     subtitle = models.CharField(max_length=100, null=True, blank=True)
-
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock()),
+            ("full_richtext", blocks.RichtextBlock()),
+        ],
+        null=True,
+        blank=True,
+    )
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
+        StreamFieldPanel("content"),
     ]
 
     class Meta:  # noqa 
---
+++ streams/blocks.py
@@ -0,0 +1,19 @@
+from wagtail.core import blocks
+
+
+class TitleAndTextBlock(blocks.StructBlock):
+   
+    # title = blocks.CharBlock(required=True, help_text="Add your title")
+    text = blocks.TextBlock(required=True, help_text="Add additional text")
+
+    class Meta:  
+        template = "streams/title_and_text_block.html"
+        icon = "edit"
+        label = "Title & Text"
+
+class RichtextBlock(blocks.RichTextBlock):
+
+    class Meta:  # noqa
+        template = "streams/richtext_block.html"
+        icon = "doc-full"
+        label = "Full RichText"        
